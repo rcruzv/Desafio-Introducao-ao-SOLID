44e61f4042f3f65595846c16e71fcce6f98493fa
Rodrigo da Cruz Vieira <rodrigocruz.vieira@portoseguro.com.br>
2021-11-20 12:38:04 -0300

    Correction: User List Users

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,15 +2,22 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User[] {
+        const user = this.usersRepository.findById(user_id);
+        if (!user) {
+            throw new Error("User not defined!");
+        }
+        if (!user.admin) {
+            throw new Error("User not Admin!");
+        }
+        return this.usersRepository.list();
+    }
 }
 
 export { ListAllUsersUseCase };
