803bf00ae4b8f558be7e389bb445e6aa626c6410
Rodrigo da Cruz Vieira <rodrigocruz.vieira@portoseguro.com.br>
2021-11-20 12:35:08 -0300

    Correction: User Create

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -2,16 +2,21 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  name: string;
-  email: string;
+    name: string;
+    email: string;
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ email, name }: IRequest): User {
+        const user = this.usersRepository.findByEmail(email);
+        if (user) {
+            throw new Error("User already exists!");
+        }
+
+        return this.usersRepository.create({ email, name });
+    }
 }
 
 export { CreateUserUseCase };
