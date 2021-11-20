07dc094c8357d4bad80e698d774cb7298c3f47e5
Rodrigo da Cruz Vieira <rodrigocruz.vieira@portoseguro.com.br>
2021-11-20 12:33:53 -0300

    Correction: User Repository

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -2,41 +2,52 @@ import { User } from "../../model/User";
 import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
 
 class UsersRepository implements IUsersRepository {
-  private users: User[];
+    private users: User[];
 
-  private static INSTANCE: UsersRepository;
+    private static INSTANCE: UsersRepository;
 
-  private constructor() {
-    this.users = [];
-  }
-
-  public static getInstance(): UsersRepository {
-    if (!UsersRepository.INSTANCE) {
-      UsersRepository.INSTANCE = new UsersRepository();
+    private constructor() {
+        this.users = [];
     }
 
-    return UsersRepository.INSTANCE;
-  }
+    public static getInstance(): UsersRepository {
+        if (!UsersRepository.INSTANCE) {
+            UsersRepository.INSTANCE = new UsersRepository();
+        }
 
-  create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
-  }
+        return UsersRepository.INSTANCE;
+    }
 
-  findById(id: string): User | undefined {
-    // Complete aqui
-  }
+    create({ name, email }: ICreateUserDTO): User {
+        const user = new User();
+        Object.assign(user, {
+            name,
+            email,
+        });
 
-  findByEmail(email: string): User | undefined {
-    // Complete aqui
-  }
+        this.users.push(user);
+        return user;
+    }
 
-  turnAdmin(receivedUser: User): User {
-    // Complete aqui
-  }
+    findById(id: string): User | undefined {
+        const user = this.users.find((user) => user.id === id);
+        return user;
+    }
 
-  list(): User[] {
-    // Complete aqui
-  }
+    findByEmail(email: string): User | undefined {
+        const user = this.users.find((user) => user.email === email);
+        return user;
+    }
+
+    turnAdmin(receivedUser: User): User {
+        const user = receivedUser;
+        user.admin = true;
+        return user;
+    }
+
+    list(): User[] {
+        return this.users;
+    }
 }
 
 export { UsersRepository };
